cfcd1fbdfc9a3b73854368681f72f2d9ccc4330d
Kristian Portillo <portillk@gmail.com>
2020-07-08 18:45:36 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -81,7 +81,12 @@ def adding_matz
         :languages => ["C"]
       }
     }
-
+programmer_hash[:yukihiro_matsumoto]
+:yukihiro_matsumoto => {
+   :known_for => "Ruby",
+   :languages => ["LISP", "C"]
+ }
+ programmer_hash
 end
 
 def changing_alan
